cffa4a710ba5852beb61fc65b66cd6ff7d93c61e
Dominik <dlunko@foi.hr>
2021-06-01 16:10:49 +0200

    dorada

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,58 +1,55 @@
 const products = [
-    {
-      name: "PlayStation 5",
-      imageUrl:
-        "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
-      description:
-        "PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4, the PS5 was released on November 12, 2020 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2020 onwards in other major markets except China and India.",
-      price: 499,
-      countInStock: 15,
-    },
-    {
-      name: "Iphone 12",
-      imageUrl:
-        "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
-      description:
-        "Welcome to a new era of iPhone. Beautifully bright 6.1-inch Super Retina XDR display.1 Ceramic Shield with 4x better drop performance.2 Incredible low-light photography with Night mode on all cameras. Cinema-grade Dolby Vision video recording, editing, and playback. Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless charging.3 Let the fun begin.",
-      price: 1099,
-      countInStock: 10,
-    },
-    {
-      name: "Cannon EOS-1D",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-      description:
-        "The EOS-1D X combines speed with image quality, to create the next generation camera for professionals. Full frame 18 megapixel sensor with Dual “DIGIC 5+” processors sets the standard, and up to 12 frames per second shooting takes it beyond.",
-      price: 1300,
-      countInStock: 5,
-    },
-    {
-      name: "Amazon Alexa",
-      imageUrl:
-        "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
-      description:
-        "It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system.",
-      price: 50,
-      countInStock: 25,
-    },
-    {
-      name: "Audio Technica Headphones",
-      imageUrl:
-        "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-      description:
-        "Outfitted with 45mm large-aperture dynamic drivers and an over-ear, closed-back design, the ATH-M50x headphones deliver clarity, deep bass, and extended bandwidth (15 Hz to 28 kHz) while isolating you from outside sounds.",
-      price: 233,
-      countInStock: 4,
-    },
-    {
-      name: "JBL FLIP 4",
-      imageUrl:
-        "https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
-      description:
-        "JBL Flip 4 is the next generation in the award-winning Flip series; it is a portable Bluetooth speaker that delivers surprisingly powerful stereo sound. This compact speaker is powered by a 3000mAh rechargeable Li-ion battery that offers up to 12 hours of continuous, high-quality audio playtime.",
-      price: 140,
-      countInStock: 10,
-    },
-  ];
-  
-  export default products;
+  {
+    name: "HUAWEI P40 Lite",
+    imageUrl:
+      "https://www.links.hr/content/images/thumbs/008/0081476_smartphone-huawei-p40-lite-64-6gb-128gb-android-10-rozi.jpg",
+    description:
+      "HUAWEI AppGallery je službena Huawei platforma za distribuciju, pretraživanje, preuzimanje, upravljanje i dijeljenje mobilnih aplikacija. Huawei nastavlja razvijati ekosustav globalnih aplikacija kako bi vam pružio osigurana, odabrana i raznolika iskustva. Uživajte u ekskluzivnom sadržaju aplikacija, lokalnim aktivnostima i posebnim ponudama.",
+    price: 1599,
+    countInStock: 10,
+  },
+  {
+    name: "APPLE iPhone 11 Pro Max",
+    imageUrl:
+      "https://www.links.hr/content/images/thumbs/007/0070783_smartphone-apple-iphone-11-pro-max-6-5-512gb-zeleni-040100341.jpg",
+    description:
+      "Apple iPhone Pro Max dolazi u mat staklenom tijelu kojeg nadopunjava kirurški čelik. S prednje strane nalazi se Retina XDR zaslon veličine 6.5“. Zaobljenih rubova u predivnoj crnoj boji ovaj iPhone donosi sasvim novu dozu elegancije u tvoj život.",
+    price: 12979,
+    countInStock: 20,
+  },
+  {
+    name: "SAMSUNG Galaxy S21",
+    imageUrl:
+      "https://www.links.hr/content/images/thumbs/009/0096702_smartphone-samsung-galaxy-s21-g991b-5g-6-2-8gb-128gb-android-11-rozi-040100787_400.jpg",
+    description:
+      "Napravljen za spektakularnu svakodnevicu Nikad više nemojte propustiti taj savršeni kadar. Dizajniran za revoluciju u videozapisima i fotografiji, s kinematografskom 8K rezolucijom, tako da možete izdvojiti epske fotografije izravno iz videozapisa. Sve ima u dvije veličine: 64MP, naš najbrži čipset i masivnu cjelodnevnu bateriju.",
+    price: 6400,
+    countInStock: 7,
+  },
+  {
+    name: "XIAOMI MI 10T",
+    imageUrl:
+      "https://www.links.hr/content/images/thumbs/009/0096017_smartphone-xiaomi-mi-10t-6-67-8gb-128gb-android-10-crni-040100769.jpg",
+    description:
+      "Vrhunska kamera, zaslon i brzina Potpuno oslobodite kreativca u sebi uz moćne karakteristike i funkcije kamer 7680 × 4320 ultra visoka rezolucija | Uslikajte 33MP slike direktno iz vaših 8K video zapisa Recite doviđenja kompleksnom korištenju - uz ovaj aparat učenici postaju učitelji",
+    price: 3499,
+    countInStock: 27,
+  },
+  {
+    name: "CAT S52",
+    imageUrl:
+      "https://www.links.hr/content/images/thumbs/010/0102335_smartphone-cat-s52-5-65-4gb-64gb-android-9-crni-040100843.jpg",
+    description: " OS Android 9 Procesor OctaCore CPU model Cortex A53 Chipset Mediatek MT6765 Helio P3 GPU Da GPU model PowerVR GE8320 Memorija (RAM) 4 GB Memorija za pohranu (GB) 64 GB",
+    price: 3799,
+    countInStock: 4,
+  },
+  {
+    name: "NOKIA 225 4G DS",
+    imageUrl:"https://www.links.hr/content/images/thumbs/009/0096718_mobitel-nokia-225-4g-ds-2-4-4g-lte-microsd-dual-sim-kamera-crni-049500041.jpg",
+    description:"Nokia 225 4G predstavlja kombinaciju klasičnoga i modernoga. Uživajte u svijetu pogodnosti koje vam se pružaju 4G mrežom poput bolje kvalitete poziva, društvenih mreža i igranja igara za više igrača. Zahvaljujući vrhunskoj učinkovitoj tipkovnici i namjenskim funkcijskim tipkama tipkanje je lakše, a navigacija još preciznija. Sjajnim i tvrdim bojama postiže se visokokvalitetna završna obrada.",
+    price: 426,
+    countInStock: 6,
+  },
+];
+
+export default products;
